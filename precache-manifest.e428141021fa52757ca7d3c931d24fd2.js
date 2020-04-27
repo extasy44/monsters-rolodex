582bdbceb151ff110a56cb676384929a3635b184
extasy44 <seoheejun@gmail.com>
2020-04-27 22:26:10 +1000

    Updates

---
+++ precache-manifest.e428141021fa52757ca7d3c931d24fd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d361d4cfe107629b1a4e0083e5b6c054",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9e54cf5a363ccb6115c8",
+    "url": "/monsters-rolodex/static/css/main.7aeacdab.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e54cf5a363ccb6115c8",
+    "url": "/monsters-rolodex/static/js/main.111bb469.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
